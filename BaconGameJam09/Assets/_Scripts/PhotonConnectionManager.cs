dc4f22796337fccb209640c642fa49dee9a74ca3
TJ Daberkow <tjdaberkow@gmail.com>
2015-06-12 21:49:33 -0500

    Added some basic photon shit

---
+++ BaconGameJam09/Assets/_Scripts/PhotonConnectionManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PhotonConnectionManager : MonoBehaviour 
+{
+    [SerializeField] private Text _playerCount;
+    [SerializeField] private GameObject _playerPrefab;
+
+	void Awake() 
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.ConnectUsingSettings(null);
+
+      //  PhotonNetworkingMessage.OnJoinedRoom
+	}
+
+    private void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+
+    private void Update()
+    {
+        _playerCount.text = PhotonNetwork.countOfPlayers.ToString();
+    }
+
+    private void OnJoinedLobby()
+    {
+        Debug.Log("Connected to Photon");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    private void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Join failed creating");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    private void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+    }
+}
---
+++ BaconGameJam09/Assets/_Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _force = 1.0f;
+
+	private void FixedUpdate()
+	{
+        var direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        _rigidbody.AddForce(direction * _force);
+	}
+}
