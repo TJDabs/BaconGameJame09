01ba264338068b163f9d953a73c93ceef28cd811
TJ Daberkow <tjdaberkow@gmail.com>
2015-06-12 23:54:57 -0500

    sadf

--- BaconGameJam09/Assets/_Scripts/CameraFollow.cs
+++ BaconGameJam09/Assets/_Scripts/CameraFollow.cs
@@ -5,9 +5,16 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _zDepth = -1;
+    private Quaternion _rotation;
+
+    private void Awake()
+    {
+        _rotation = transform.rotation;
+    }
 
 	private void Update ()
     {
         transform.position = new Vector3(_target.position.x, _target.position.y, _target.position.z - _zDepth);
+        transform.rotation = _rotation;
 	}
 }
