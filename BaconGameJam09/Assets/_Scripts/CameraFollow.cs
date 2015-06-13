e5420ea1e389b3919a02b4bbf2ed5bbbdb0833cd
TJ Daberkow <tjdaberkow@gmail.com>
2015-06-12 23:44:54 -0500

    Camera Following

---
+++ BaconGameJam09/Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _zDepth = -1;
+
+	private void Update ()
+    {
+        transform.position = new Vector3(_target.position.x, _target.position.y, _target.position.z - _zDepth);
+	}
+}
